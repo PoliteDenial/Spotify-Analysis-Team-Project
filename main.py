003a486c590a96f7aa5cb1128c5a4d72363bc60e
Avery Fulton <avfu5764@colorado.edu>
2022-04-12 16:21:35 -0600

    Added Ranking with avg ranking

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+
+st.title("Whats trending in Denver?")
+st.write("We used the spotify api to get the top unique songs in Denver over the span of the last few months.")
+
+
+st.write("This is a graph of the top songs in Denver over the last few months.")
